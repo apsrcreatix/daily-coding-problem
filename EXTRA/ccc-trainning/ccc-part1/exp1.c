3200484a67becde47bb77c3e96dff47ba6c748cc
Aditya Pratap Singh Rajput <apsrcreatix@gmail.com>
2019-05-05 09:25:48 +0530

    remove old stuff

--- EXTRA/ccc-trainning/ccc-part1/exp1.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(){
-  int res;
-  res=printf("aditya");
-  printf("\n%d\n",res );
-  return 0;
-}
--- EXTRA/ccc-trainning/ccc-part1/pointers.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#define num(x) (size(x)/size(*x))
-int _tmain(){
-  int number[10]={0};
-  printf("%d\n",num(number) );
-}
